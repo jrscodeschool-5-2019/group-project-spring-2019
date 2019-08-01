4f9e2f21f8c03a86accac62e32c47085e2349590
Zachary Van Slambrook <zvanslambrook@yahoo.com>
2019-08-01 12:50:00 -0400

    nav bar moved to own file, hamburger menu now working

--- src/studentPage.js
+++ src/studentPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import Logo from './JRS_Coding_School_logo.png'
 import 'bulma/css/bulma.css'
 import CardList from './components/card-list/CardList'
+import NavMenu from './navMenu'
 
 function Page() {
   const [users, setUsers] = useState([])
@@ -16,49 +16,27 @@ function Page() {
     // Navagation section and logo
 
     <div>
-      <nav className='navbar'>
-        <div className='container'>
-          <div className='navbar-brand'>
-            <a href='../'>
-              <img src={Logo} alt='Logo' width='250px' />
-            </a>
-            <span className='navbar-burger burger' data-target='navbarMenu'>
-              <span />
-              <span />
-              <span />
-            </span>
-          </div>
-          <div id='navbarMenu' className='navbar-menu'>
-            <div className='navbar-end'>
-              <a className='navbar-item is-active'>Home</a>
-              <a className='navbar-item'>something</a>
-              <a className='navbar-item'>something</a>
-              <a className='navbar-item'>something</a>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <NavMenu />
 
       {/* end of the nav section , search section below */}
-
-      <div className='container'>
-        <div id='flow'>
-          <span className='flow-1' />
-          <span className='flow-2' />
-          <span className='flow-3' />
+      <div className="container">
+        <div id="flow">
+          <span className="flow-1" />
+          <span className="flow-2" />
+          <span className="flow-3" />
         </div>
-        <div className='section'>
-          <div className='box'>
-            <div className='field has-addons'>
-              <div className='control is-expanded'>
+        <div className="section">
+          <div className="box">
+            <div className="field has-addons">
+              <div className="control is-expanded">
                 <input
-                  className='input has-text-centered'
-                  type='search'
-                  placeholder='Search Users'
+                  className="input has-text-centered"
+                  type="search"
+                  placeholder="Search Users"
                 />
               </div>
-              <div className='control'>
-                <a className='button is-info'>Search</a>
+              <div className="control">
+                <a className="button is-info">Search</a>
               </div>
             </div>
           </div>
