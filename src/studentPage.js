7f500090b4e711148cc44fb9197154a93179e12d
tydillon <harrell_t@icloud.com>
2019-08-05 13:49:34 -0400

    adding the events sidebar

--- src/studentPage.js
+++ src/studentPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import 'bulma/css/bulma.css'
 import CardList from './components/card-list/CardList'
 import NavMenu from './navMenu'
+import SideBar from './components/sidebar/Sidebar'
 
 function Page() {
   const [users, setUsers] = useState([])
@@ -17,36 +18,40 @@ function Page() {
 
     <div>
       <NavMenu />
-
-      {/* end of the nav section , search section below */}
-      <div className="container">
-        <div id="flow">
-          <span className="flow-1" />
-          <span className="flow-2" />
-          <span className="flow-3" />
-        </div>
-        <div className="section">
-          <div className="box">
-            <div className="field has-addons">
-              <div className="control is-expanded">
-                <input
-                  className="input has-text-centered"
-                  type="search"
-                  placeholder="Search Users"
-                />
-              </div>
-              <div className="control">
-                <a className="button is-info">Search</a>
+      <div className='columns'>
+        <SideBar />
+        <div className='column is-8'>
+          {/* end of the nav section , search section below */}
+          <div className='container'>
+            <div id='flow'>
+              <span className='flow-1' />
+              <span className='flow-2' />
+              <span className='flow-3' />
+            </div>
+            <div className='section'>
+              <div className='box'>
+                <div className='field has-addons'>
+                  <div className='control is-expanded'>
+                    <input
+                      className='input has-text-centered'
+                      type='search'
+                      placeholder='Search Users'
+                    />
+                  </div>
+                  <div className='control'>
+                    <a className='button is-info'>Search</a>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+
+          {/* End of the search */}
+
+          {/* columns for student cards */}
+          <CardList users={users} className='has-text-centered' />
         </div>
       </div>
-
-      {/* End of the search */}
-
-      {/* columns for student cards */}
-      <CardList users={users} />
     </div>
   )
 }
