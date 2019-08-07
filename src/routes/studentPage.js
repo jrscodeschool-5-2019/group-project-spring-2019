f60e664c056056a46fb4fcd0c97d8faeef8c4429
Zachary Van Slambrook <zvanslambrook@yahoo.com>
2019-08-07 12:38:44 -0400

    Cleaning up studen page

--- src/routes/studentPage.js
+++ src/routes/studentPage.js
@@ -1,62 +1,56 @@
-import React, {useEffect, useState} from 'react';
-import 'bulma/css/bulma.css';
-import CardList from '../components/card-list/CardList';
-import SearchDirectory from '../components/search-directory/SearchDirectory';
-import SideBar from '../components/sidebar/Sidebar';
+import React, { useEffect, useState } from 'react'
+import 'bulma/css/bulma.css'
+import CardList from '../components/card-list/CardList'
+import SearchDirectory from '../components/search-directory/SearchDirectory'
+import SideBar from '../components/sidebar/Sidebar'
 
 const Page = props => {
-  const [users, setUsers] = useState([]);
-  const [search, setSearch] = useState('');
+  const [users, setUsers] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     fetch('http://localhost:8000/student-view')
       .then(res => res.json())
-      .then(data => setUsers(data.data));
-  }, []);
+      .then(data => setUsers(data.data))
+  }, [])
 
   const handleChange = e => {
-    setSearch(e.target.value);
-  };
+    setSearch(e.target.value)
+  }
 
   const filteredUsers = users.filter(
     user =>
       user.name.first.toLowerCase().includes(search.toLowerCase()) +
       user.name.last.toLowerCase().includes(search.toLowerCase())
-  );
+  )
 
   return (
     <div>
-      <div className='columns'>
+      <div className="columns">
+        {/* search bar */}
         <SideBar />
-        <div className='column is-9'>
-          <div className='container'>
-            <div id='flow'>
-              <span className='flow-1' />
-              <span className='flow-2' />
-              <span className='flow-3' />
-            </div>
-            <div className='section'>
-              <div className='box'>
-                <div className='field has-addons'>
-                  <div className='control is-expanded'>
+        {/* Search bar section */}
+        <div className="column is-9">
+          <div className="container">
+            <div className="section">
+              <div className="box">
+                <div className="field has-addons">
+                  <div className="control is-expanded">
                     <SearchDirectory handleChange={handleChange} />
                   </div>
-                  <div className='control'>
-                    <button className='button is-info'>Search</button>
+                  <div className="control">
+                    <button className="button is-info">Search</button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          {/* Cards called in here */}
           <CardList users={filteredUsers} />
         </div>
       </div>
-
-      {/* End of the search */}
-
-      {/* columns for student cards */}
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
