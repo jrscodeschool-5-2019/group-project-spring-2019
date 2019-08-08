4a0705bd2108a74ca83b89580a8d9538fe70d008
Michael Greco <michael.greco07@gmail.com>
2019-08-08 11:46:56 -0400

    2nd commit a lot of stuff

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { Router } from '@reach/router'
-import LandingPage from './routes/landingPage'
-import Registration from './routes/registration-form'
-import Login from './routes/login-page'
-import Page from './routes/studentPage'
-import Profile from './routes/profilePage'
-import NavMenu from './components/navMenu'
+import React, { useState, useEffect } from "react";
+import { Router } from "@reach/router";
+import LandingPage from "./routes/landingPage";
+import Registration from "./routes/registration-form";
+import Login from "./routes/login-page";
+import Page from "./routes/studentPage";
+import Profile from "./routes/profilePage";
+import NavMenu from "./components/navMenu";
 
 function App() {
-  const [user, setUser] = useState({ loggedIn: false, username: '' })
+  const [user, setUser] = useState({ loggedIn: false, username: "" });
 
-  useEffect(() => getUser(), [])
+  useEffect(() => getUser(), []);
 
-  const updateUser = userObject => setUser(userObject)
+  const updateUser = userObject => setUser(userObject);
 
   // getUser is not really working (backend issue I think)
   const getUser = () => {
-    fetch('http://localhost:8000/user/', {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
+    fetch("http://localhost:8000/user/", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
     })
       .then(response => response.json())
-      .then(res => console.log(res))
+      .then(res => console.log(res));
     // .then(res => {
     //   if (res.user) {
     //     console.log('Get User: There is a user saved in the server session: ');
@@ -39,21 +39,21 @@ function App() {
     //     });
     //   }
     // });
-  }
+  };
 
   return (
     <div>
       <NavMenu user={user} updateUser={updateUser} />
 
       <Router>
-        <LandingPage path='/' />
-        <Registration path='/registration' />
-        <Login path='/student-login' updateUser={updateUser} />
-        <Page path='/student-view' />
-        <Profile path='/profile' user={user} />
+        <LandingPage path="/" />
+        <Registration path="/registration" />
+        <Login path="/student-login" updateUser={updateUser} />
+        <Page path="/student-view" />
+        <Profile path="/profile" user={user} />
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/routes/profilePage.js
+++ src/routes/profilePage.js
@@ -1,10 +1,266 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { redirectTo } from "@reach/router";
 
-const Profile = props => {
+const Profile = ({ user }) => {
+  const [img, setImg] = useState("");
+  const [gradYear, setGradYear] = useState("");
+  const [employer, setEmployer] = useState("");
+  const [bio, setBio] = useState("");
+  const [contactLinks, setContactLinks] = useState({
+    gitHub: "",
+    linkedIn: "",
+    other: ""
+  });
+  const [finalProject, setFinalProject] = useState("");
+  const [name, setName] = useState({ first: "", last: "" });
+  const [currentStudent, setCurrentStudent] = useState(false);
+  const [seekingEmployment, setSeekingEmployment] = useState(false);
+  const [location, setLocation] = useState("");
+
+  const handleImg = e => {
+    setImg(e.target.value);
+  };
+
+  const handleGradYear = e => {
+    setGradYear(e.target.value);
+  };
+
+  const handleEmployer = e => {
+    setEmployer(e.target.value);
+  };
+
+  const handleBio = e => {
+    setBio(e.target.value);
+  };
+
+  const handleFinalProject = e => {
+    setFinalProject(e.target.value);
+  };
+
+  const handleLocation = e => {
+    setLocation(e.target.value);
+  };
+
+  const handleName = e => {
+    setName({ ...name, [e.target.id]: e.target.value });
+  };
+
+  const handleContactLinks = e => {
+    setContactLinks({ ...contactLinks, [e.target.id]: e.target.value });
+  };
+
+  const handleEmployerStatus = e => {
+    setSeekingEmployment(e.target.checked);
+  };
+
+  const handleCurrentStudent = e => {
+    setCurrentStudent(e.target.checked);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const formData = {
+      name: name,
+      contactLinks: contactLinks,
+      currentStudent: currentStudent,
+      seekingEmployment: seekingEmployment,
+      img: img,
+      gradYear: gradYear,
+      employer: employer,
+      bio: bio,
+      finalProject: finalProject,
+      location: location
+    };
+    fetch("");
+  };
+
+  useEffect(() => {
+    if (user.loggedIn) {
+      redirectTo("/student-login");
+    }
+    fetch("http://localhost:8000/profile/5d4b2b1b9fbba62a69792845")
+      .then(res => res.json())
+      .then(data => {
+        setImg(data.img);
+        setGradYear(data.gradYear);
+        setEmployer(data.employer);
+        setBio(data.bio);
+        setContactLinks(data.contactLinks);
+        setFinalProject(data.finalProject);
+        setName(data.name);
+        setCurrentStudent(data.currentStudent);
+        setSeekingEmployment(data.seekingEmployment);
+        setLocation(data.location);
+      });
+  }, []);
+
+  // const handleChange = (e) => {
+  //   setProfile({...profile, [e.target.id]: e.target.value})
+  // }
+
   return (
     <div>
-      This is a placeholder profile page!
-      {props.user.loggedIn && <p>Welcome, {props.user.username}!</p>}
+      <img src={img} alt={img} />
+      <div>{name.first}</div>
+      <div className="field">
+        <label className="label">Profile Picture</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="img URL"
+            value={img}
+            onChange={handleImg}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Graduation Year</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="Year"
+            value={gradYear}
+            onChange={handleGradYear}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Employer</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="Employer"
+            value={employer}
+            onChange={handleEmployer}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Bio</label>
+        <div className="control">
+          <textarea
+            className="textarea"
+            type="text"
+            placeholder="Short Bio"
+            value={bio}
+            onChange={handleBio}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Final Project</label>
+        <div className="control">
+          <textarea
+            className="textarea"
+            type="text"
+            placeholder="Final Project"
+            value={finalProject}
+            onChange={handleFinalProject}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Location</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="Location"
+            value={location}
+            onChange={handleLocation}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">First Name</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="First Name"
+            id="first"
+            value={name.first}
+            onChange={handleName}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Last Name</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="Last Name"
+            id="last"
+            value={name.last}
+            onChange={handleName}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Github</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="Github"
+            id="gitHub"
+            value={contactLinks.gitHub}
+            onChange={handleContactLinks}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">LinkedIn</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="LinkedIn"
+            id="linkedIn"
+            value={contactLinks.linkedIn}
+            onChange={handleContactLinks}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Other</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            placeholder="Other"
+            id="other"
+            value={contactLinks.other}
+            onChange={handleContactLinks}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="checkbox">
+          <input
+            type="checkbox"
+            checked={currentStudent}
+            onChange={handleCurrentStudent}
+          />
+          Current Students
+        </label>
+      </div>
+      <div className="field">
+        <label className="checkbox">
+          <input
+            type="checkbox"
+            checked={seekingEmployment}
+            onChange={handleEmployerStatus}
+          />
+          Seeking Employment
+        </label>
+      </div>
+      <button className="button" onClick={handleSubmit} />
+      Submit
     </div>
   );
 };
