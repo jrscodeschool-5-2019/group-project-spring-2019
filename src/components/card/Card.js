84b396c41c6655d65cf29084c8afa16c9fcf4308
kevinleemoody <Kevin.moody98@gmail.com>
2019-08-09 14:29:54 -0400

    center cards

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,87 +1,93 @@
-
-import React from 'react'
-import './card-styles.css'
-import 'bulma/css/bulma.css'
+import React from "react";
+import "./card-styles.css";
+import "bulma/css/bulma.css";
 
 const Card = ({ user }) => {
   const toggleModal = e => {
-    const toggleModal = document.getElementById(`${user._id}`)
-    toggleModal.classList.toggle('is-active')
-  }
-  let jobStatus = ""
+    const toggleModal = document.getElementById(`${user._id}`);
+    toggleModal.classList.toggle("is-active");
+  };
+  let jobStatus = "";
   if (user.seekingEmployment == false) {
-    jobStatus = "Not seeking employment"
-  
-}
-  else{
-    jobStatus = "seeking employment"
+    jobStatus = "Not seeking employment";
+  } else {
+    jobStatus = "seeking employment";
   }
 
-    return (
+  return (
+    <div>
+      <div className="box card">
+        <center>
+          <img className="avatar" alt={`${user._id} img`} src={user.img} />
+          <br />
+          <h1>
+            <strong>
+              {user.name.first} {user.name.last}
+            </strong>
+          </h1>
+          <h3>{user.employer}</h3>
+          <br />
+          <h3>
+            <span className="label">Graduation Year:</span> {user.gradYear}
+          </h3>
+          <br />
+          <h3>
+            <span className="label">Current Location:</span>
+            {user.location}
+          </h3>
+          <span>
+            <p>
+              <button className="button" onClick={toggleModal}>
+                See More
+              </button>
+            </p>
+          </span>
+        </center>
+      </div>
 
-
-     <div>     
-    <div className="box card">
-      <img className="avatar" alt={`${user._id} img`} src={user.img} />
-      <br />
-      <h1>
-        <strong>
-          {user.name.first} {user.name.last}
-        </strong>
-      </h1>
-      <h3>{user.employer}</h3>
-      <br />
-      <h3>
-        <span className="label">Graduation Year:</span> {user.gradYear}
-      </h3>
-      <br />
-      <h3>
-        <span className="label">Current Location:</span>
-        {user.location}
-      </h3>
-      <span>
-
-        
-<p>  
-  <button className='button' onClick={toggleModal}>
-    See More
-  </button>
-  </p>
-</span>
-</div>
-   <div className='modal' id={user._id}>
-        <div className='modal-background'>
-          <div className='modal-card'>
-            <header className='modal-card-head'>
-              <p className='modal-card-title'>{user.name.first} {user.name.last}</p>
+      <div className="modal" id={user._id}>
+        <div className="modal-background">
+          <div className="modal-card">
+            <header className="modal-card-head">
+              <p className="modal-card-title">
+                {user.name.first} {user.name.last}
+              </p>
               <button
-                className='delete'
-                aria-label='close'
+                className="delete"
+                aria-label="close"
                 onClick={toggleModal}
               />
             </header>
-            <section className='modal-card-body'>
-            
-            <center><div class="column" >
-            <img className="avatar"   alt={`${user._id} img`} src={user.img} /> <br />
-            {user.employmentStatus} <br />
-            {user.employer} <br />
-            {user.bio}
-            <h3>
-        <a href={user.contactLinks.gitHub}>Github</a> |
-        <a href={user.contactLinks.linkedIn}> LinkedIn </a> |
-        <a href={user.contactLinks.other}> Other</a> <br />
-       
-         <span className="label">Current Location:</span>{user.location} 
-         <span className='label'>Graduation Year: </span> {user.gradYear} 
-         <span className='label'>Final Project:</span>{user.finalProject}
-         <span className='label'>Employment Status: </span> {jobStatus}
-           </h3>
-           </div>
-            </center>
+            <section className="modal-card-body">
+              <center>
+                <div class="column">
+                  <img
+                    className="avatar"
+                    alt={`${user._id} img`}
+                    src={user.img}
+                  />{" "}
+                  <br />
+                  {user.employmentStatus} <br />
+                  {user.employer} <br />
+                  {user.bio}
+                  <h3>
+                    <a href={user.contactLinks.gitHub}>Github</a> |
+                    <a href={user.contactLinks.linkedIn}> LinkedIn </a> |
+                    <a href={user.contactLinks.other}> Other</a> <br />
+                    <span className="label">Current Location:</span>
+                    {user.location}
+                    <span className="label">Graduation Year: </span>{" "}
+                    {user.gradYear}
+                    <span className="label">Final Project:</span>
+                    {user.finalProject}
+                    <span className="label">Employment Status: </span>{" "}
+                    {jobStatus}
+                  </h3>
+                </div>
+              </center>
             </section>
-            <footer className='modal-card-foot'>
-             <br />
+            <footer className="modal-card-foot">
+              <br />
             </footer>
           </div>
         </div>
